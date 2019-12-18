44d6d3523614b667a1dfa2a50cd16c39fe1cd610
giusha3116 <giorgiokroshidze3@gmail.com>
2019-12-18 04:56:22 +0200

    v,rs,rest proof

--- exam/roles/mysql/templates/database.sql
+++ exam/roles/mysql/templates/database.sql
@@ -1,5 +1,5 @@
 create database if not exists wordpress;
 use wordpress;
-create user if not exists wordpress@'%' identified by '{{ secret }}';
-grant all on wordpress to wordpress@'%';
+create user if not exists '{{ wordpress_user }}'@'%' identified by '{{ wordpress_password }}';
+grant all on wordpress to '{{ wordpress_user }}'@'%';
 flush privileges;
